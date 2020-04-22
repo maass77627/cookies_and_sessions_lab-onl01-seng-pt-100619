f2c49ff1ce88934ea60490f094485164f4f629ff
kelly r maass <kmaass77627@gmail.com>
2020-04-22 14:00:03 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   get 'product/index'
 
   get 'product/add'
 
-=======
 root "products#index"
 post "/" => "products#add"
 #resources :products, only: [:show]
->>>>>>> 9054d04e1206634dcc98eddb2c3894a8a88a161a
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
